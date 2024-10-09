91bc4651306455f44c2849f499d8a9bce437931a
hieptuan.thai <hieptuan.thai@tungstenautomation.com>
2024-10-09 16:18:12 +0700

    C Lab 5.5

---
+++ 1. C labs/Lab 5/Bai5_Lab5/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("size of char: %d bit\n", sizeof(char));
+    printf("size of int: %d bit\n", sizeof(int));
+    printf("size of long: %d bit\n", sizeof(long));
+    printf("size of long long: %d bit\n", sizeof(long long));
+    printf("size of float: %d bit\n", sizeof(float));
+    printf("size of double: %d bit\n", sizeof(double));
+    return 0;
+}
