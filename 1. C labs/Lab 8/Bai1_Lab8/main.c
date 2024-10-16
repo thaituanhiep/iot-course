bdedc2f8858e537e3aee47d408fe7d002ef37c64
hieptuan.thai <hieptuan.thai@tungstenautomation.com>
2024-10-16 10:41:03 +0700

    C Lab 8

---
+++ 1. C labs/Lab 8/Bai1_Lab8/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < length; i++) {
+        printf("%d\t", arr[i]);
+    }
+    return 0;
+}
